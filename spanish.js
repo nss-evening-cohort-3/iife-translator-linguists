73c8dfaa64f8e0eba6c6209cdd22f43aea649d07
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-25 15:30:48 -0600

    Basic functionality of a spanish translator that accepts a string from the console and outputs it to the console

--- spanish.js
+++ spanish.js
@@ -16,43 +16,52 @@ var MasterTranslator = function(originalMasterTranslator) {
     return passedEnglishText.split(" ");
   }
 
+////  
+// This function accepts an English Array and...
+//  1) Makes a temporary array to hold the new spanish phrase
+//  2) Cycles through each element in the passed English array
+//  3) Cycles through each key in the Spanish Language Lexicon
+//  4) Checks to see if the current English Array element equals the current Spanish Lexicon Key
+//   a) If there is a match, the english word gets replaced with the spanish word.
+//  5) Returns the new Spanish Array
   function checkAndReplaceEnglishArray(passedEnglishArray) {
-    var newSpanishArray = [];
 
+//  1) Makes a temporary array to hold the new spanish phrase
+    var tempSpanishString = "";
+
+//  2) Cycles through each element in the passed English array
     passedEnglishArray.forEach(function(englishWord) {
+
+//  3) Cycles through each key in the Spanish Language Lexicon
       Object.keys(spanishLexicon).forEach(function(lexiconKey) {
         
+//  4) Checks to see if the current English Array element equals the current Spanish Lexicon Key
+//   a) If there is a match, the english word gets replaced with the spanish word into a new string.
         if (englishWord.includes(lexiconKey)) {
-          newSpanishArray += englishWord.replace(spanishLexicon[lexiconKey], spanishLexicon[lexiconKey]) + " ";
+          tempSpanishString += englishWord.replace(lexiconKey, spanishLexicon[lexiconKey]) + " ";
         };
       });
      });
 
-    return newSpanishArray;
+//  5) Returns the new Spanish Array
+    return tempSpanishString;
   };
-
-
-  //   Object.keys(obj).forEach(function(element) {
-  //   console.log(element === "1");
-  // })
 
 // This handles the translating of the text entered in the DOM
 // The argument "translateText" is whatever variable we assign the translate-text 
 //  div pull in the DOM.
   originalMasterTranslator.translateToSpanish = function(translateText) {
 
-    //The string from the DOM needs to be split into individual words 
-    // (dump to array)
+    //The string from the DOM needs to be split into individual words (dump to array)
     var englishArrayToBeTranslated = makeEnglishArray(translateText);
-    
-    checkAndReplaceEnglishArray(englishArrayToBeTranslated);
-    
+
     //The individual words need to be checked and replaced with appropriate spanish word
     //  Build a new array using forEach() 
-
-
-
+    var spanishString = checkAndReplaceEnglishArray(englishArrayToBeTranslated);
+    
     // Output new array to div DOM element.
+    console.log(spanishString);
+
   }
 
 // This just returns the Spanish Lexicon but keeps the spanishLexicon object 
