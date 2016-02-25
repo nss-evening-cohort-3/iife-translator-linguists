d235076eee84ce92140f9030e4aa5177ee1949e5
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-24 23:19:56 -0600

    added spanish.js file and spanish lexicon

---
+++ spanish.js
@@ -0,0 +1,13 @@
+var MasterTranslator = function() {
+
+  var spanishLexicon = {
+    englishLanguage: ["merry", "christmas", "and", "happy", "new", "year"],
+    spanishLanguage: ["feliz", "navidad", "y", "próspero", "nuevo", "año"]
+  }
+
+  console.log(spanishLexicon.englishLanguage[0]);
+  console.log(spanishLexicon.spanishLanguage[0]);
+
+
+
+}( MasterTranslator || {} );
