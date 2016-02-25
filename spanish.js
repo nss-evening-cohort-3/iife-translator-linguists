b6d5d77296e65d5d167a572a0afb69c79488cbec
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-25 14:39:51 -0600

    added basic array to replace lowercase english words with spanish

--- spanish.js
+++ spanish.js
@@ -11,17 +11,46 @@ var MasterTranslator = function(originalMasterTranslator) {
     "year": "año",
   }
 
+// Simply takes the english string passed from the DOM and builds an Array out of it. 
+  function makeEnglishArray(passedEnglishText) {
+    return passedEnglishText.split(" ");
+  }
+
+  function checkAndReplaceEnglishArray(passedEnglishArray) {
+    var newSpanishArray = [];
+
+    passedEnglishArray.forEach(function(englishWord) {
+      Object.keys(spanishLexicon).forEach(function(lexiconKey) {
+        
+        if (englishWord.includes(lexiconKey)) {
+          newSpanishArray += englishWord.replace(spanishLexicon[lexiconKey], spanishLexicon[lexiconKey]) + " ";
+        };
+      });
+     });
+
+    return newSpanishArray;
+  };
+
+
+  //   Object.keys(obj).forEach(function(element) {
+  //   console.log(element === "1");
+  // })
+
 // This handles the translating of the text entered in the DOM
 // The argument "translateText" is whatever variable we assign the translate-text 
 //  div pull in the DOM.
   originalMasterTranslator.translateToSpanish = function(translateText) {
 
     //The string from the DOM needs to be split into individual words 
-    // (dump to array, check for punctuation)
-    createEnglishArray(translateText);
-
+    // (dump to array)
+    var englishArrayToBeTranslated = makeEnglishArray(translateText);
+    
+    checkAndReplaceEnglishArray(englishArrayToBeTranslated);
+    
     //The individual words need to be checked and replaced with appropriate spanish word
     //  Build a new array using forEach() 
+
+
 
     // Output new array to div DOM element.
   }
@@ -31,11 +60,6 @@ var MasterTranslator = function(originalMasterTranslator) {
   originalMasterTranslator.getSpanishLexicon = function() {
     return spanishLexicon;
   }
-
-
-
-
-
 
   return originalMasterTranslator;
 
