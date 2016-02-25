7d4c881747e525cb994813c4f031637d5c0a4235
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-25 08:44:49 -0600

    added getLexicon Function and commented which sub functions are needed for translation

--- spanish.js
+++ spanish.js
@@ -1,13 +1,42 @@
-var MasterTranslator = function() {
+var MasterTranslator = function(originalMasterTranslator) {
 
+// Private object in English:Spanish Key:Value pairs.  Since these are strings you have to
+//  access them using bracket notation (i.e. spanishLexicon["merry"] yields "feliz")
   var spanishLexicon = {
-    englishLanguage: ["merry", "christmas", "and", "happy", "new", "year"],
-    spanishLanguage: ["feliz", "navidad", "y", "próspero", "nuevo", "año"]
+    "merry": "feliz",
+    "christmas": "navidad",
+    "and": "y",
+    "happy": "próspero",
+    "new": "nuevo",
+    "year": "año",
   }
 
-  console.log(spanishLexicon.englishLanguage[0]);
-  console.log(spanishLexicon.spanishLanguage[0]);
+// This handles the translating of the text entered in the DOM
+// The argument "translateText" is whatever variable we assign the translate-text 
+//  div pull in the DOM.
+  originalMasterTranslator.translateToSpanish = function(translateText) {
+
+    //The string from the DOM needs to be split into individual words 
+    // (dump to array, check for punctuation)
+    createEnglishArray(translateText);
+
+    //The individual words need to be checked and replaced with appropriate spanish word
+    //  Build a new array using forEach() 
+
+    // Output new array to div DOM element.
+  }
+
+// This just returns the Spanish Lexicon but keeps the spanishLexicon object 
+//  private and non-editable.
+  originalMasterTranslator.getSpanishLexicon = function() {
+    return spanishLexicon;
+  }
 
 
+
+
+
+
+  return originalMasterTranslator;
 
 }( MasterTranslator || {} );
