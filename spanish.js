752cbd281aff957b250496d5388151191794b217
Bernard Anderson <bernard.anderson@gmail.com>
2016-02-25 22:57:22 -0600

    Added ability to capitalize first letter or whole word if english word is capped

--- spanish.js
+++ spanish.js
@@ -16,14 +16,9 @@ var MasterTranslator = function(originalMasterTranslator) {
     return passedEnglishText.split(" ");
   }
 
-////  
+////
 // This function accepts an English Array and...
-//  1) Makes a temporary array to hold the new spanish phrase
-//  2) Cycles through each element in the passed English array
-//  3) Cycles through each key in the Spanish Language Lexicon
-//  4) Checks to see if the current English Array element equals the current Spanish Lexicon Key
-//   a) If there is a match, the english word gets replaced with the spanish word.
-//  5) Returns the new Spanish Array
+////
   function checkAndReplaceEnglishArray(passedEnglishArray) {
 
 //  1) Makes a temporary array to hold the new spanish phrase
@@ -33,19 +28,31 @@ var MasterTranslator = function(originalMasterTranslator) {
     passedEnglishArray.forEach(function(englishWord) {
 
 //  3) Cycles through each key in the Spanish Language Lexicon
-      Object.keys(spanishLexicon).forEach(function(lexiconKey) {
+      Object.keys(spanishLexicon).forEach(function(lexiconEnglishWord) {
         
-//  4) Checks to see if the current English Array element equals the current Spanish Lexicon Key
-//   a) If there is a match, the english word gets replaced with the spanish word into a new string.
-        if (englishWord.includes(lexiconKey)) {
-          tempSpanishString += englishWord.replace(lexiconKey, spanishLexicon[lexiconKey]) + " ";
+//  4) Checks to see if the current English Array element equals the current Spanish Lexicon Key if there is a 
+//   match...
+//   a) the english word gets checked to see if it's all caps and then is replaced with the spanish word in all caps
+//   b) the english word gets checked to see if cap first letter and is replaced with the spanish word with first 
+//       cap letter.
+//   c) if both a and b fail, the english word is replaced with lower case spanish word
+        if (englishWord.toLowerCase().includes(lexiconEnglishWord)) {
+            var currentLexiconEnglishWord = spanishLexicon[lexiconEnglishWord];
+          if (englishWord.toUpperCase() === englishWord) {
+            tempSpanishString += englishWord.toLowerCase().replace(lexiconEnglishWord, currentLexiconEnglishWord.toUpperCase()) + " ";
+          } else if (englishWord[0].toUpperCase() === englishWord.substring(0,1)) {
+            var capitalizedSpanishWord = currentLexiconEnglishWord.substring(0,1).toUpperCase() + currentLexiconEnglishWord.substring(1);
+            tempSpanishString += englishWord.toLowerCase().replace(lexiconEnglishWord, capitalizedSpanishWord) + " ";
+          } else {
+            tempSpanishString += englishWord.replace(lexiconEnglishWord, currentLexiconEnglishWord) + " ";
+          };
         };
       });
-     });
-
+    });
 //  5) Returns the new Spanish Array
     return tempSpanishString;
-  };
+  }
+
 
 // This handles the translating of the text entered in the DOM
 // The argument "translateText" is whatever variable we assign the translate-text 
