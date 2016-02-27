c25f7a535cc046c1010893cef6644fd61701a636
Janelle Zorvan <jzorvan@gmail.com>
2016-02-27 11:19:56 -0600

    added french lex getter and updated main.js

--- french.js
+++ french.js
@@ -8,15 +8,17 @@ var MasterTranslator = function(originalMasterTranslator) {
     "happy": "bonne",
     "new": "nouvelle",
     "year": "année",
+    };
+
+  originalMasterTranslator.getFrenchLexicon = function() {
+    return frenchLexicon;
   }
 
   var translatedSentence;
   var resultArea;
 
-  originalMasterTranslator.translateToFrench = function(){
+  originalMasterTranslator.translateToFrench = function(capUserInput){
 
-    var capUserInput= document.getElementById("input-field").value.toUpperCase();
-    // var capUserInput= "merry christmas and happy new year".toUpperCase();
     var userArray = capUserInput.split(" ");
 
     userArray[userArray.indexOf("MERRY")] = frenchLexicon["merry"];
@@ -26,34 +28,15 @@ var MasterTranslator = function(originalMasterTranslator) {
     userArray[userArray.indexOf("NEW")] = frenchLexicon["new"];
     userArray[userArray.indexOf("YEAR")] = frenchLexicon["year"];
 
-    console.log("userArray", userArray);
-
     translatedSentence = userArray.reduce(function(previous, current) {
       return previous + " " + current;
-
       });
+
     translatedSentence = translatedSentence + ".";
       console.log("translatedSentence", translatedSentence);
-        
+      return translatedSentence;
     }
-
-    resultArea = document.getElementById("translate-text");
-    resultArea.innerHTML += "<div>" + translatedSentence + "</div>";
-    // etc...
-
-
-    // Notes:
-    // Join up the result and pump it to the DOM element?  .reduce?
-    //  var transSent = ...(userArray);
-    //     return transSent
-    // }
-
-    // originalMasterTranslator... = function(userArray) {
-    //     var outputSent = userArray.reduce();
-    //     return outputSent
-    // }
-
 
   return originalMasterTranslator;
 
-}( MasterTranslator || {} );
+} ( MasterTranslator || {} );
