eaba3f54b0ff4d53979ae23cb38b9f1f5ed8c33c
Janelle Zorvan <jzorvan@gmail.com>
2016-02-29 15:37:11 -0600

    Styled page, added styling classes to html, changed french js very poorly

--- french.js
+++ french.js
@@ -1,6 +1,9 @@
 
 var MasterTranslator = function(originalMasterTranslator) {
 
+  var translatedSentence = [];
+  var resultArea;
+
   var frenchLexicon = {
     "merry": "joyeux",
     "christmas": "Noël",
@@ -15,33 +18,21 @@ var MasterTranslator = function(originalMasterTranslator) {
     return frenchLexicon;
   }
 
-  var translatedSentence;
-  var resultArea;
+  originalMasterTranslator.translateToFrench = function(userInput) {
 
-  originalMasterTranslator.translateToFrench = function(userInput){
+    var userArray = userInput.toLowerCase().split(" ");
 
-    var userArray = userInput.split(" ");
+    userArray.forEach(function(word) {
+      translatedSentence.push(frenchLexicon[word]);
+    });
 
-  originalMasterTranslator.translateToFrench = function(userInput){
-
-    var userArray = userInput.toUpperCase().split(" ");
-
-    userArray[userArray.indexOf("MERRY")] = frenchLexicon["merry"];
-    userArray[userArray.indexOf("CHRISTMAS")] = frenchLexicon["christmas"];
-    userArray[userArray.indexOf("AND")] = frenchLexicon["and"];
-    userArray[userArray.indexOf("HAPPY")] = frenchLexicon["happy"];
-    userArray[userArray.indexOf("NEW")] = frenchLexicon["new"];
-    userArray[userArray.indexOf("YEAR")] = frenchLexicon["year"];
-    userArray[userArray.indexOf("MONKEYBUTT")] = frenchLexicon["monkeybutt"];
-
-    translatedSentence = userArray.reduce(function(previous, current) {
+    translatedSentence = translatedSentence.reduce(function(previous, current) {
       return previous + " " + current;
-      });
+    });
 
-    translatedSentence = translatedSentence + ".";
-      console.log("translatedSentence", translatedSentence);
-      return translatedSentence;
-    }
+    return translatedSentence;
+
+    };
 
   return originalMasterTranslator;
 
